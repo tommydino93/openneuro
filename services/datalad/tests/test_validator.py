f452b21d2ac3e966bb65ddd16a73ae67ed8da12a
Nell Hardcastle <nell@squishymedia.com>
2020-10-08 15:46:28 -0700

    Add GitLab CI build script.

--- services/datalad/tests/test_validator.py
+++ services/datalad/tests/test_validator.py
@@ -1,9 +1,13 @@
 import json
+import shutil
+
+import pytest
 
 from .dataset_fixtures import *
 from datalad_service.tasks.validator import validate_dataset_sync
 
 
+@pytest.mark.skipif(not shutil.which('yarn'), reason="yarn must be installed for validation tests")
 def test_validator(new_dataset):
     results = validate_dataset_sync(new_dataset.path, 'HEAD')
     # new_dataset doesn't pass validation, should return an error
